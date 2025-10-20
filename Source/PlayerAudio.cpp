18581901c487de6fcdb0697af3d3a6c46a25c91f
ahmed tolba <atolba388@gmail.com>
2025-10-20 16:22:44 +0300

    project structutre

---
+++ Source/PlayerAudio.cpp
@@ -0,0 +1,85 @@
+﻿/*
+  ==============================================================================
+
+    PlayerAudio.cpp
+    Created: 20 Oct 2025 2:41:11pm
+    Author:  Lenovo LOQ
+
+  ==============================================================================
+*/
+
+#include "PlayerAudio.h"
+
+PlayerAudio::PlayerAudio()
+{
+	formatManager.registerBasicFormats();
+
+}
+
+PlayerAudio::~PlayerAudio()
+{
+}
+
+void PlayerAudio::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void PlayerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void PlayerAudio::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+bool PlayerAudio::loadFile(const juce::File& file)
+{
+    if (file.existsAsFile())
+    {
+        if (auto* reader = formatManager.createReaderFor(file))
+        {
+            // 🔑 Disconnect old source first
+            transportSource.stop();
+            transportSource.setSource(nullptr);
+            readerSource.reset();
+
+            // Create new reader source
+            readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+
+            // Attach safely
+            transportSource.setSource(readerSource.get(),
+                0,
+                nullptr,
+                reader->sampleRate);
+            transportSource.start();
+        }
+    }
+	return true;
+}
+void PlayerAudio::start()
+{
+    transportSource.start();
+}
+void PlayerAudio::stop()
+{
+    transportSource.stop();
+}
+void PlayerAudio::setGain(float gain)
+{
+    transportSource.setGain(gain);
+}
+void PlayerAudio::setPosition(double pos)
+{
+    transportSource.setPosition(pos);
+}
+double PlayerAudio::getPosition() const
+{
+    return transportSource.getCurrentPosition();
+}
+double PlayerAudio::getLength() const
+{
+    return transportSource.getLengthInSeconds();
+}
