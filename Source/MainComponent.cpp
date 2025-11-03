13fda0a1ad62e70e76f526883a3535deec4c4399
ahmed tolba <atolba388@gmail.com>
2025-11-03 17:44:53 +0200

    player2

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,7 @@
 MainComponent::MainComponent()
 {
 	addAndMakeVisible(player1);
+	addAndMakeVisible(player2);
     setSize(800, 350);  // Increased height to accommodate metadata at top
     setAudioChannels(0, 2);
 }
@@ -15,21 +16,25 @@ MainComponent::~MainComponent()
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
     player1.prepareToPlay(samplesPerBlockExpected, sampleRate);
+	player2.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
     player1.getNextAudioBlock(bufferToFill);
+	player2.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
 {
     player1.releaseResources();
+	player2.releaseResources();
 }
 
 void MainComponent::resized()
 {
 	player1.setBounds(getLocalBounds());
+	player2.setBounds(getLocalBounds().withTop(getHeight() / 2).withHeight(getHeight() / 2));
 }
 
 
